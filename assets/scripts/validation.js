fa74be1fdf72194a3119a8c3ba095b02f09b2cd1
JesusJimenez2107 <jjimenez_23@alu.uabcs.mx>
2025-10-15 13:07:52 -0700

    A10- Formulario de registro con validación en vivo

--- assets/scripts/validation.js
+++ assets/scripts/validation.js
@@ -1,24 +1,48 @@
 function onlyLetters(event) {
     const pressedKey = event.key;
+    const input = event.target;
+
     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
         'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         'Á', 'É', 'Í', 'Ó', 'Ú',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
         'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'á', 'é', 'í', 'ó', 'ú',' '];
+        'á', 'é', 'í', 'ó', 'ú', ' '];
 
-    if (!letters.includes(pressedKey)){
-        return false
-    }    
+    if (!letters.includes(pressedKey)) {
+        event.preventDefault();
+        input.classList.add("error");
+        setTimeout(() => input.classList.remove("error"), 500);
+        return false;
+    }
+    return true;
 }
 
 function onlyNumbers(event) {
     const pressedKey = event.key;
+    const input = event.target;
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    if (!numbers.includes(pressedKey)){
-        return false
-    }    
+    if (!numbers.includes(pressedKey)) {
+        event.preventDefault();
+        input.classList.add("error");
+        setTimeout(() => input.classList.remove("error"), 500);
+        return false;
+    }
+    return true;
+}
+
+function noSpace(event) {
+    const pressedKey = event.key;
+    const input = event.target;
+
+    if (pressedKey === ' ') {
+        event.preventDefault();
+        input.classList.add("error");
+        setTimeout(() => input.classList.remove("error"), 500);
+        return false;
+    }
+    return true;
 }
 
 function validarCorreo() {
